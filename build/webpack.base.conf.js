8100d894db95f82d2d26ef0aa7afee1e0d6f1a5f
jack huang <hwguge6@gmail.com>
2019-07-16 19:55:05 +0800

    add header

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -36,6 +36,8 @@ module.exports = {
     alias: {
       'vue$': 'vue/dist/vue.esm.js',
       '@': resolve('src'),
+      // 创建一个别名 
+      'styles': resolve('src/assets/styles'),
     }
   },
   module: {
