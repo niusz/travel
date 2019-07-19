92a25ad10a44f34c4a837ed4fc24221db7966e70
jack huang <hwguge6@gmail.com>
2019-07-19 14:26:50 +0800

    performance

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -2,8 +2,15 @@
   <div>
    <city-header></city-header>
    <city-search></city-search>
-   <city-list :cities="cities" :hot='hotCities'></city-list>
-   <city-alphabet :cities="cities"></city-alphabet>
+   <city-list 
+   :cities="cities" 
+   :hot='hotCities'
+   :letter="letter">
+   </city-list>
+   <city-alphabet 
+   :cities="cities"
+   @change="handleLetterChange">
+   </city-alphabet>
   </div>
 </template>
 <script>
@@ -23,7 +30,8 @@ export default {
     data () {
         return {
             cities: {},
-            hotCities: []
+            hotCities: [],
+            letter: ''
         }
     },
     methods: {
@@ -38,7 +46,11 @@ export default {
                 this.cities = data.cities
                 this.hotCities = data.hotCities
             }
+        },
+        handleLetterChange (letter) {
+            this.letter = letter
         }
+
     },
     mounted () {
         this.getCityInfo ()
